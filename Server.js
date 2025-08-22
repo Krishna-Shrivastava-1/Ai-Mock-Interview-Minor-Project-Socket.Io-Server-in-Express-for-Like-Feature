7785a6d5698b0ac1f948e53dedbf86e68cf2d22c
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-08-23 01:50:27 +0530

    added real time follow and unfollow socket

--- Server.js
+++ Server.js
@@ -38,6 +38,29 @@ io.on('connection', (socket) => {
         }
     });
 
+
+// followunfollow
+        socket.on("followUser", async ({ followerId, followingId }) => {
+        try {
+            console.log('followerId', followerId);
+            console.log('followingId', followingId);
+            
+            // Call your Next.js API for follow/unfollow
+            const response = await axios.put("https://skillsync-ebon.vercel.app/api/auth/followandunfollow", {
+                followerId: followerId,
+                followingId: followingId
+            });
+
+            const updatedProfiles = response?.data;
+
+            // Emit an event to all clients with the updated profiles
+            io.emit("userProfilesUpdated", updatedProfiles);
+
+        } catch (err) {
+            console.error("Error following user:", err);
+        }
+    });
+
     socket.on("disconnect", () => {
         console.log("User disconnected:", socket.id);
     });
